ed60c88b5b7b6aa3ca84fa2a2dac7ef7bdde5cdf
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-02-27 09:43:16 +0200

    Created the emoji picker modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,14 @@ const Button = ({ label, theme, onPress }) => {
     if (theme == "primary") {
         return (
             <View style={styles.buttonContainer}>
-                <Pressable style={[styles.button, { backgroundColor: "white", padding: 5 , borderWidth: 2 , borderColor: "gold", borderRadius: 10 }]} onPress={onPress}
-                    onPressIn={() => alert('You pressed a button.')}
-                >
+                <Pressable style={[styles.button, { backgroundColor: "white", padding: 5, borderWidth: 2, borderColor: "gold", borderRadius: 10 }]} onPress={onPress}>
                     <FontAwesome
                         size={18}
                         name="picture-o"
                         color="#25292e"
                         style={styles.buttonIcon}
                     />
-                    <Text style={[ styles.buttonLabel, { color: 'black'} ]}>{label}</Text>
+                    <Text style={[styles.buttonLabel, { color: 'black' }]}>{label}</Text>
                 </Pressable>
             </View>
         )
@@ -22,7 +20,7 @@ const Button = ({ label, theme, onPress }) => {
 
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button')}>
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
